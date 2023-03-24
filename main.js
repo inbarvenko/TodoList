dc9031d12d79a94248aadeeeb85ba2bf23f55bee
inbarvenko <Inga_barvenko@mail.ru>
2023-03-24 17:21:27 +0300

    d number of tasks

--- main.js
+++ main.js
@@ -9,96 +9,131 @@
 // -- сохранять стиль для текста при перезагрузке (???) 
 
 
+class EventList {
+  listeners = [];
+
+  subscribe(cb) {
+    if (this.listeners.includes(cb)) { return }
+    this.listeners.push(cb)
+  }
+
+  notify(state) {
+    this.listeners.forEach((listener) => {
+      listener(state);
+    })
+  }
+
+  remove(cb) {
+    this.listeners = this.listeners.filter((listener) => listener !== cb);
+  }
+}
+
+
+
 const textInput = document.getElementById('input_text');
 const addButton = document.getElementById('add_button');
 
 let toDoList_page = document.getElementById('todo');
-
 let toDoList = [];
 
+const currentTasks = () => {
+  const list = document.querySelector('.list__current');
+  const num = document.querySelector('.number');
+  if (num) {
+    num.innerHTML= toDoList.length;
+  }
+  else list.insertAdjacentHTML('beforeend', `<h2 class="number">${toDoList.length}</h2>`);
+}
+
+
 const showToDo = () => {
-    let strItem = '';
-    toDoList.forEach((item, index) => {
-         strItem += `
+  let strItem = '';
+  toDoList.forEach((item, index) => {
+    strItem += `
         <li class="items__item">
             <input id="item_${index}" type="checkbox" class="item__check" ${item.done ? 'checked' : ''}>
             <p for="item_${index}" class="item__text">${item.task}</p>
             <button class="item__delete-button">X</button>
         </li>
         `;
-        toDoList_page.innerHTML = strItem;
-    });
+    toDoList_page.innerHTML = strItem;
+  });
+  currentTasks();
 }
 
 //  LocalStorage для сохранение инфы на локал сервере
-if(localStorage.getItem('todo')){
-    toDoList = JSON.parse(localStorage.getItem('todo'));
-    showToDo();
+if (localStorage.getItem('todo')) {
+  toDoList = JSON.parse(localStorage.getItem('todo'));
+  showToDo();
 };
 
 const addNewTask = () => {
 
-    for(let item of toDoList){
-        if(item.task == textInput.value){
-            return ;
-        }
+  for (let item of toDoList) {
+    if (item.task == textInput.value) {
+      return;
     }
+  }
 
-    let newTask = {
-        task: textInput.value,
-        done: false
-    };
+  if (textInput.value == '') return;
 
-    toDoList.push(newTask);
-    showToDo();
-    localStorage.setItem('todo', JSON.stringify(toDoList));
+  let newTask = {
+    task: textInput.value,
+    done: false
+  };
+
+  toDoList.push(newTask);
+  showToDo();
+  localStorage.setItem('todo', JSON.stringify(toDoList));
 }
 
 addButton.addEventListener('click', () => addNewTask());
 
 textInput.addEventListener('keypress', (event) => {
-    if(event.code == 'Enter' && textInput.value != ''){
-        addNewTask();
-    }
+  if (event.code == 'Enter' && textInput.value != '') {
+    addNewTask();
+  }
 });
 
 const containerList = document.querySelector('.container__list');
 
-containerList.addEventListener('click', (event) => {
-    switch(event.target.className){
+const containerEvent = (event) => {
+  switch (event.target.className) {
 
-        case 'item__delete-button': 
-            let item = event.target.closest('.items__item');
-            item.remove();
+    case 'item__delete-button':
+      let item = event.target.closest('.items__item');
+      item.remove();
 
-            const value = item.children[1].innerHTML;
-            
-            toDoList.pop({task: value});
-            localStorage.setItem('todo', JSON.stringify(toDoList));
-            break;
+      const value = item.children[1].innerHTML;
 
-        case 'item__check':
+      toDoList.pop({ task: value });
+      currentTasks()
+      localStorage.setItem('todo', JSON.stringify(toDoList));
+      break;
 
-            let text = event.target.nextElementSibling;
+    case 'item__check':
 
-            let value_ch = event.target.nextElementSibling.innerHTML;
-            for(let item of toDoList){ 
-                if(item.task == value_ch && !item.done){
-                    item.done = true;
+      let text = event.target.nextElementSibling;
 
-                    text.style.cssText = `
+      let value_ch = event.target.nextElementSibling.innerHTML;
+      for (let item of toDoList) {
+        if (item.task == value_ch && !item.done) {
+          item.done = true;
+
+          text.style.cssText = `
                         color: grey;
                         text-decoration: line-through;
                     `;
-                }
-                else if (item.task == value_ch && item.done){
-                    item.done = false;
-                    text.style.cssText = '';
-                }
-            }
+        }
+        else if (item.task == value_ch && item.done) {
+          item.done = false;
+          text.style.cssText = '';
+        }
+      }
 
-            localStorage.setItem('todo', JSON.stringify(toDoList));
-            break;
-        default: return;
-    }
-});
+      localStorage.setItem('todo', JSON.stringify(toDoList));
+      break;
+    default: return;
+  }
+}
+containerList.addEventListener('click', containerEvent);
