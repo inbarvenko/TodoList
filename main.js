e1a0b65d5635f64977c985e0fcd7f78444da7da3
inbarvenko <Inga_barvenko@mail.ru>
2023-03-24 15:19:26 +0300

    delete and add

--- main.js
+++ main.js
@@ -5,25 +5,59 @@ let toDoList_page = document.getElementById('todo');
 
 let toDoList = [];
 
+
 const showToDo = () => {
     let strItem = '';
     toDoList.forEach((item, index) => {
          strItem += `
-        <li>
-            <input id="item_${index}" type="checkbox" ${item.done ? 'checked' : ''}>
-            <p for="item_${index}">${item.task}</p>
+        <li class="items__item">
+            <input id="item_${index}" type="checkbox" class="item__check" ${item.done ? 'checked' : ''}>
+            <p for="item_${index}" class="item__text">${item.task}</p>
+            <button class="item__delete-button">X</button>
         </li>
         `;
         toDoList_page.innerHTML = strItem;
     });
 }
 
-addButton.addEventListener("click", () => {
+// -- LocalStorage для сохранение инфы на локал сервере
+localStorage.setItem('todo', JSON.stringify(toDoList));
+if(localStorage.getItem('todo')){
+    toDoList = JSON.parse(localStorage.getItem('todo'));
+    showToDo();
+};
+
+const addNewTask = () => {
+    // -- проверка на сущестование такого таска
+
     let newTask = {
         task: textInput.value,
         done: false
     };
 
     toDoList.push(newTask);
-    showToDo()
+    showToDo();
+}
+
+addButton.addEventListener('click', () => addNewTask());
+
+textInput.addEventListener('keypress', (event) => {
+    // -- проверить на поле из пробелов
+    if(event.code == 'Enter' && textInput.value != ''){
+        addNewTask();
+    }
+});
+
+
+const containerList = document.querySelector('.container__list');
+
+containerList.addEventListener('click', (event) => {
+    if (event.target.className != 'item__delete-button') return;
+
+    let item = event.target.closest('.items__item');
+    item.remove();
+
+    const value = item.children[1].innerHTML;
+    
+    toDoList.pop({task: value});
 });
